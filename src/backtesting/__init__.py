ce8f55857cccc5338c27c5d3a46ad71e61f0874e
oakwoodgates <wpguru4u@gmail.com>
2026-03-12 20:15:50 -0400

    exports

--- src/backtesting/__init__.py
+++ src/backtesting/__init__.py
@@ -1,3 +1,9 @@
-from src.backtesting.engine import make_engine, run_single_backtest, run_sweep
+from src.backtesting.engine import (
+    load_sweeps,
+    make_engine,
+    run_single_backtest,
+    run_sweep,
+    run_walk_forward,
+)
 
-__all__ = ["make_engine", "run_single_backtest", "run_sweep"]
+__all__ = ["load_sweeps", "make_engine", "run_single_backtest", "run_sweep", "run_walk_forward"]
